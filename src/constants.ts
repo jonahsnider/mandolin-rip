9ce61ed6f65be96e563e9e36024f8e6fc20cca4b
Kevin Mahoney <716806+kevcube@users.noreply.github.com>
2021-09-28 09:34:28 -0600

    remove code that enables piracy 😉

--- src/constants.ts
+++
@@ -1,5 +0,0 @@
-import path from 'node:path';
-
-export const BASE_DIR = path.join(__dirname, '..');
-export const BASE_DOWNLOADS_DIR = path.join(BASE_DIR, 'downloads');
-export const BASE_STITCHED_DIR = path.join(BASE_DIR, 'stitched');
